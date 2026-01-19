0df1497d58333bf2e307a22abfe63a21a296051e
Felipe <minelyne@gmail.com>
2026-01-18 21:15:58 -0300

    fix anchor issues

--- src/pages/Builder.tsx
+++ src/pages/Builder.tsx
@@ -5,15 +5,15 @@ import { ControlPanel } from '../components/builder/ControlPanel';
 
 const Builder: React.FC = () => {
     return (
-        <div className="h-screen flex text-white overflow-hidden">
+        <div className="h-screen w-full flex text-white overflow-hidden relative">
             {/* Sidebar Palette */}
             <PaletteSidebar />
 
             {/* Main Canvas */}
             <MainCanvas />
 
-            {/* Control Panel */}
-            <div className="w-80 bg-zinc-900 border-l border-zinc-700 p-4 overflow-y-auto custom-scrollbar">
+            {/* Control Panel (Anchored to the right) */}
+            <div className="w-80 h-full bg-zinc-900/95 border-l border-zinc-700 p-4 overflow-y-auto custom-scrollbar shadow-2xl z-20">
                 <ControlPanel />
             </div>
         </div>
